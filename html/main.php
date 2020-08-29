ed9c087674fa6dbebefe171bc14b98d267565842
tjrehdrms123 <wwe5455@naver.com>
2020-08-29 16:08:23 +0900

    중복되는 부분 php 로 묵기

--- html/main.php
+++ html/main.php
@@ -1,165 +1,320 @@
 <div class="main-content">
-	<ul id="img-slide">
-		<li class="img-box">
-			<img src="images/slide/slide01.png" alt="img1"> 
-		</li>
-		<li class="img-box">
-			<img src="images/slide/slide02.png" alt="img2">
-		</li>
-		<li class="img-box">
-			<img src="images/slide/slide03.png" alt="img3">
-		</li>
-		<li class="img-box">
-			<img src="images/slide/slide04.png" alt="img4">
-		</li>	
-		<li class="img-box">
-			<img src="images/slide/slide05.png" alt="img5">
-		</li>	
-		<li class="img-box">
-			<img src="images/slide/slide06.png" alt="img6">
-		</li>	
-		<li class="img-box">
-			<img src="images/slide/slide07.png" alt="img7">
-		</li>	
-		<li class="img-box">
-			<img src="images/slide/slide08.png" alt="img8">
-		</li>	
-		<li class="img-box">
-			<img src="images/slide/slide09.png" alt="img9">
-		</li>																			
-	</ul>
+    <ul id="img-slide">
+        <li class="img-box">
+            <img src="images/slide/slide01.png" alt="img1">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide02.png" alt="img2">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide03.png" alt="img3">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide04.png" alt="img4">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide05.png" alt="img5">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide06.png" alt="img6">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide07.png" alt="img7">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide08.png" alt="img8">
+        </li>
+        <li class="img-box">
+            <img src="images/slide/slide09.png" alt="img9">
+        </li>
+    </ul>
     <?php
     $str = array(
        array(
-        'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'title' => '<온라인단독>',
+        'sub_title' => '컬러 블로킹 아노락 자켓',
+        'price' => 129000,
+        'sqle' => '99000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>',
+        'sub_title' => '헤리티지 블로킹 자켓',
+        'price' => 129000,
+        'sqle' => '99000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>',
+        'sub_title' => '팔레트 보아 플리스 자켓',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '팝콘 보아 플리스 자켓',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '팔레트 보아 플리스 자켓',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '팔레트 보아 플리스 자켓',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '팝콘 보아 플리스 자켓',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '팝콘 보아 플리스 자켓',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '오버핏 스몰리니어 기모 후디',
+        'price' => 69000,
+        'sqle' => '49000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '오버핏 스몰리니어 기모 후디',
+        'price' => 69000,
+        'sqle' => '49000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '휠라레이',
+        'price' => 59000,
+        'sqle' => '49000 (17% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '레이 트레이서',
+        'price' => 69000,
+        'sqle' => '59000 (12% ↓)'
+       ),
+       // 2번째 영역
+       array(
+        'title' => '<온라인단독>',
+        'sub_title' => '휠라골프 PQ 티셔츠',
+        'price' => 129000,
+        'sqle' => '99000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>',
+        'sub_title' => '휠라골프 PQ 티셔츠',
+        'price' => 129000,
+        'sqle' => '99000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>',
+        'sub_title' => '휠라골프 제원단에리 티셔츠',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '휠라골프 PQ 카라티',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
        array(
         'title' => '<온라인단독>FILAECH 등판 로고',
-        'sub_title' => '루츠핏 반팔 티셔츠',
-        'price' => 32000,
-        'sqle' => '24000 (25% ↓)'
+        'sub_title' => '휠라골프 PQ 카라티',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
        ),
+       array(
+        'title' => '<온라인단독>FILAECH 등판 로고',
+        'sub_title' => '라이언 남성 티셔츠',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>FILAECH 등판 로고',
+        'sub_title' => '라이언 남성 티셔츠',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>FILAECH 등판 로고',
+        'sub_title' => '컬러블럭 여성 티셔츠',
+        'price' => 99000,
+        'sqle' => '79000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>FILAECH 등판 로고',
+        'sub_title' => '크롭 여성 티셔츠',
+        'price' => 69000,
+        'sqle' => '49000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>FILAECH 등판 로고',
+        'sub_title' => '크롭 여성 티셔츠',
+        'price' => 69000,
+        'sqle' => '49000 (25% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>FILAECH 등판 로고',
+        'sub_title' => '키즈 바시티 레이어드 후디',
+        'price' => 59000,
+        'sqle' => '49000 (17% ↓)'
+       ),
+       array(
+        'title' => '<온라인단독>FILAECH 등판 로고',
+        'sub_title' => '키츠 사이드 빅 F 로고 맨투맨',
+        'price' => 69000,
+        'sqle' => '59000 (12% ↓)'
+       ),        
     );
-    ?>  	
-   <!-- one drress-box -->
-   <p class="title">BEST ITEM</p>
+    ?>
+    <!-- one drress-box -->
+    <p class="title">BEST ITEM</p>
     <?php 
-        for( $i = 0; $i<count($str);  $i++ ){
+        for( $i = 0; $i<count($str)/2;  $i++ ){
             $num = sprintf('%02d',($i+1));
-    ?>	
-	<div class="dress-box">		
-		<div>
-			<img src="images/dress/dress<?php echo $num?>.jpg" alt="">
-		</div>
-		<div class="dress-con">
-			 <?php echo $str[$i]['title'] ?><br>
-			 <?php echo $str[$i]['sub_title'] ?><br>
-			 <span class="normal"><?php echo $str[$i]['price'] ?></span>
-			 <span class="sale"><?php echo $str[$i]['sqle'] ?></span>
-			 <div class="colorchip">
-			 	<button class="btn-black"></button>
-			 	<button class="btn-green"></button>
-			 	<button class="btn-pink"></button>
-			 </div>
-		</div>
-	</div>	
-	<?php 
+    ?>
+    <div class="dress-box">
+        <div>
+            <img src="images/dress/dress<?php echo $num?>.jpg" alt="">
+        </div>
+        <div class="dress-con">
+            <?php echo $str[$i]['title'] ?><br>
+            <?php echo $str[$i]['sub_title'] ?><br>
+            <span class="normal"><?php echo $str[$i]['price'] ?></span>
+            <span class="sale"><?php echo $str[$i]['sqle'] ?></span>
+            <div class="colorchip">
+                <button class="btn-black"></button>
+                <button class="btn-green"></button>
+                <button class="btn-pink"></button>
+            </div>
+        </div>
+    </div>
+    <?php 
     }
 	?>
-	<!-- one drress-box -->
-	
-	<!-- two drress-box -->
-	<div class="subbanner">
-		<div>
-			<img src="images/subbanner/subbanner01.jpg" alt="">
-		</div>
-	</div>							
-	<p class="title">FILA</p>	
+    <!-- one drress-box -->
+
+    <!-- two drress-box -->
+    <div class="subbanner">
+        <div>
+            <img src="images/subbanner/subbanner01.jpg" alt="">
+        </div>
+    </div>
+    <p class="title">FILA</p>
     <?php 
-        for( $i = 0; $i<count($str);  $i++ ){
+        for( $i = 12; $i<count($str);  $i++ ){
             $num = sprintf('%02d',($i+1));
-    ?>	
-	<div class="dress-box">		
-		<div>
-			<img src="images/dress/dress<?php echo $num?>.jpg" alt="">
-		</div>
-		<div class="dress-con">
-			 <?php echo $str[$i]['title'] ?><br>
-			 <?php echo $str[$i]['sub_title'] ?><br>
-			 <span class="normal"><?php echo $str[$i]['price'] ?></span>
-			 <span class="sale"><?php echo $str[$i]['sqle'] ?></span>
-			 <div class="colorchip">
-			 	<button class="btn-black"></button>
-			 	<button class="btn-green"></button>
-			 	<button class="btn-pink"></button>
-			 </div>
-		</div>
-	</div>	
-	<?php 
+    ?>
+    <div class="dress-box">
+        <div>
+            <img src="images/dress/dress<?php echo $num?>.jpg" alt="">
+        </div>
+        <div class="dress-con">
+            <?php echo $str[$i]['title'] ?><br>
+            <?php echo $str[$i]['sub_title'] ?><br>
+            <span class="normal"><?php echo $str[$i]['price'] ?></span>
+            <span class="sale"><?php echo $str[$i]['sqle'] ?></span>
+            <div class="colorchip">
+                <button class="btn-black"></button>
+                <button class="btn-green"></button>
+                <button class="btn-pink"></button>
+            </div>
+        </div>
+    </div>
+    <?php 
     }
 	?>
     <!-- two drress-box -->
+
+    <!-- form-box -->
+    <?php 
+    $str2 = array(
+        array(
+            'title' => '<온라인단독>FILAECH 등판 로고',
+            'price' => 32000,
+            'price-sqle' => '25,000원 (PAYCO 첫결제)'
+        )
+    )
+    ?>
+    <div class="item-box">
+        <div class="item-des">
+            <div class="item-img-box">
+                <img src="./images/dress/dressbig01.png">
+            </div>
+            <div class="item-des-box">
+                <p class="item-des-title">
+                    컬러 블로킹 아노락 자켓
+                </p>
+                <hr>
+                <div class="item-des">
+                    <p>판매가</p>
+                    <span class="item-total">29,000원</span>
+                    <br>
+                    <p>구매혜택가</p>
+                    <span>25,000원 (PAYCO 첫결제)</span>
+                    <br>
+                    <p>구매혜택</p>
+                    <span>
+                        <p>신규가입 쿠폰 10,000원 바로 사용가능</p>
+                    </span>
+                    <br>
+                    <p>예상적립</p>
+                    <span>회원등급에 따라 2-3% 적립</span>
+                    <br>
+                    <p>상품리뷰</p>
+                    <span>6개 리뷰 | ★★★★★</span>
+                    <br>
+                    <p>인기도</p>
+                    <span>7,130(조회수) 327(판매+장바구니+관심상품)</span>
+                </div>
+            </div>
+        </div>
+        <form action="<?php ?>" method="post" class="dress-submit">
+            <div class="id-box">
+                <p>아이디</p>
+                <input type="text" name="name" placeholder="아이디를 입력해주세요"/>
+            </div>
+            <div class="password-box">
+                <p>비밀번호</p>
+                <input type="password" name="password" placeholder="비밀번호를 입력해주세요" />
+            </div>
+            <div class="item-list-box">
+                <p>상품명</p>
+                <select>
+                    <?php 
+                            for( $i = 0; $i<count($str);  $i++ ){
+                                $num = sprintf('%02d',($i+1));
+                    ?>
+                    <option value="<?php echo $str[$i]['sub_title']?>"><?php echo $str[$i]['sub_title'] ?></option>
+                    <?php 
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="color-box">
+                <p>색상</p>
+                <select>
+                    <option value="black">검정</option>
+                    <option value="green">초록</option>
+                    <option value="pink">핑크</option>
+                </select>
+            </div>
+            <div class="submit-btn">
+                <button type="submit">구매하기</button>
+                <button type="button" class="delete-btn">닫기</button>
+            </div>
+        </form>        
+    </div>
+
+    <!-- form-box -->
 </div>
