2b0de42c513af4b7dd1e27a0b5fa17d55f0c6893
tjrehdrms123 <wwe5455@naver.com>
2020-08-29 16:07:51 +0900

    js 파일 통합 , js 팝업 추가

--- html/index.php
+++ html/index.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
 <!--main.js -->
 <script src="js/jquery-1.12.3.js"></script>
-<script src="js/slide.js"></script>
+<script src="js/main.js"></script>
 <!--<script src="./js/main.js"></script>-->
 <!--main.js -->
 <!-- font -->
--- html/js/main.js
+++ html/js/main.js
@@ -1,49 +1,22 @@
-window.addEventListener('load',function(){
-	window.setInterval(imgSlide,2000);
+$(document).ready(function(){
+    // 슬라이드 이미지
+    $('.img-box').eq(0).siblings().hide();
+    var slideIndex = 0;
+    setInterval(function(){        
+        if( slideIndex < 8 ){
+            slideIndex ++;
+        }else{
+            slideIndex = 0;
+        }
+        $('.img-box').eq(slideIndex).fadeIn();
+        $('.img-box').eq(slideIndex).siblings().fadeOut();
+    },3000);
+    
+    // 팝업창
+    $('.dress-box').click(function(){
+       $('.item-box').fadeIn()
+    });
+    $('.delete-btn').click(function(){
+        $('.item-box').fadeOut()
+    });
 });
-
-function imgSlide(){
-	var on = document.querySelector('.on');
-	var elImg = document.querySelectorAll('#img-slide li > img');
-		if( elImg[0].className === 'on' ){
-			elImg[0].removeAttribute('class');
-			elImg[1].setAttribute('class','on');
-		}
-		else if( elImg[1].className === 'on' ){
-			elImg[1].removeAttribute('class');
-			elImg[2].setAttribute('class','on');				 
-		}	
-		else if( elImg[2].className === 'on' ){
-			elImg[2].removeAttribute('class');
-			elImg[3].setAttribute('class','on');						
-		}
-		else if( elImg[3].className === 'on' ){
-			elImg[3].removeAttribute('class');
-			elImg[4].setAttribute('class','on');						
-		}
-		else if( elImg[4].className === 'on' ){
-			elImg[4].removeAttribute('class');
-			elImg[5].setAttribute('class','on');						
-		}
-		else if( elImg[5].className === 'on' ){
-			elImg[5].removeAttribute('class');
-			elImg[6].setAttribute('class','on');						
-		}
-		else if( elImg[6].className === 'on' ){
-			elImg[6].removeAttribute('class');
-			elImg[7].setAttribute('class','on');						
-		}
-		else if( elImg[7].className === 'on' ){
-			elImg[7].removeAttribute('class');
-			elImg[8].setAttribute('class','on');						
-		}
-		else if( elImg[8].className === 'on' ){
-			elImg[8].removeAttribute('class');
-			elImg[0].setAttribute('class','on');						
-		}			
-};
-/*
-    * /석동근/ 이미지 슬라이드 *
-	img 태그를 읽어와 on 이라는 class를 부여하는 형식입니다
-	이미지가 추가되면 JS도 변경되어서 추후 가변적으로 변경되도록 수정 및 변경해야합니다
-*/
